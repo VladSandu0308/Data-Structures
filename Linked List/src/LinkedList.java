f941e631f6f1e9f08b87306e1acd5e2f9069f206
VladSandu0308 <44501718+VladSandu0308@users.noreply.github.com>
2020-10-28 16:08:26 +0200

    Sum o 2 LinkedLists addes

--- Linked List/src/LinkedList.java
+++ Linked List/src/LinkedList.java
@@ -13,6 +13,14 @@ class Node {
 public class LinkedList {
     Node head;
 
+    LinkedList() {
+        head = null;
+    }
+
+    LinkedList(Node n) {
+        head = n;
+    }
+
     void addLast(int d) {
         Node last = new Node(d);
         if (head == null) {
@@ -94,29 +102,75 @@ public class LinkedList {
         System.out.println();
     }
 
+    LinkedList partionList(Node n, int part) {
+        Node min = n, max = n;
+
+        while (n != null) {
+            Node next = n.next;
+            if (n.data < part) {
+                n.next = min;
+                min = n;
+            } else {
+                max.next = n;
+                max = n;
+
+            }
+            n = next;
+        }
+
+        max.next = null;
+        return new LinkedList(min);
+
+    }
+
+    Node sumList (Node n1, Node n2, int carry) {
+        if (n1 == null && n2 == null && carry == 0) return null;
+
+        int val = carry;
+        if (n1 != null) {
+            val += n1.data;
+        }
+        if (n2 != null) {
+            val += n2.data;
+        }
+
+        Node n3 = new Node(val % 10);
+
+        carry = (val > 9) ? 1 : 0;
+        n3.next = sumList(n1 == null ? null : n1.next,
+                            n2 == null ? null : n2.next, carry);
+
+        return n3;
+    }
+
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
 
         list.addLast(2);
         list.addLast(3);
-        list.addLast(24);
+        list.addLast(4);
         list.printLL();
         list.delete(3);
         list.printLL();
         list.addLast(2);
         list.addLast(3);
-        list.addLast(24);
+        list.addLast(4);
         list.removeDuplicates();
         list.printLL();
 
         list.addLast(2);
         list.addLast(3);
-        list.addLast(24);
+        list.addLast(5);
         list.kthToLast(list.head,4);
         list.deleteNode(list.head.next.next.next);
         list.printLL();
 
+        LinkedList partition = list.partionList(list.head, 10);
+        partition.printLL();
+
+        LinkedList sum = new LinkedList(list.sumList(partition.head, partition.head, 0));
+        sum.printLL();
 
     }
 }
